bbe13db84cc27c836303f64a85c14c1f51ebae57
CuWO4 <2455747695@qq.com>
2023-10-09 22:16:08 +0800

    现在程序能正确处理注释所在行了.

--- transform.cpp
+++ transform.cpp
@@ -12,6 +12,7 @@ void transform(void) {
     std::string tmp = "";
     int in_bracket = 0;
     bool in_single_quote = false, in_double_quote = false;
+    bool at_begin = true;
     for (unsigned i = 0; i < g_text.length(); i++) { // handle the comment
         if (g_text[i] == '(') in_bracket++;
         if (g_text[i] == ')') in_bracket--;
@@ -20,16 +21,22 @@ void transform(void) {
         if (g_text[i] == '\"' && i > 0 && g_text[i - 1] != '\\') in_double_quote = !in_double_quote;
         if (i + 1 < g_text.length()) {
             if (g_text[i] == '/' && g_text[i + 1] == '/') {
-                tmp += '\n';
-                for (int j = 0; j < g_space_count[g_line]; j++) tmp += ' ';
+                if(!at_begin) {
+                    tmp += '\n';
+                    for (int j = 0; j < g_space_count[g_line]; j++) tmp += ' ';
+                }
+                
                 for (; i < g_text.length() && g_text[i] != '\n'; i++) {
                     tmp += g_text[i];
                     g_char_count[g_line]++;
                 }
             }
             if (g_text[i] == '/' && g_text[i + 1] == '*') {
-                tmp += '\n';
-                for (int j = 0; j < g_space_count[g_line]; j++) tmp += ' ';
+                if(!at_begin) {
+                    tmp += '\n';
+                    for (int j = 0; j < g_space_count[g_line]; j++) tmp += ' ';
+                }
+                
                 for (; i < g_text.length() - 1 &&
                     !(g_text[i] == '*' && g_text[i + 1] == '/'); i++) {
                     tmp += g_text[i];
@@ -39,6 +46,7 @@ void transform(void) {
                 
             }
         }
+        if (g_text[i] != ' ') at_begin = false;
         if (is_control_char(g_text[i])) {
             if (in_single_quote || in_double_quote) {
                 tmp += g_text[i];
@@ -79,6 +87,7 @@ void transform(void) {
         else if (g_text[i] == '\n') {
             g_line++;
             g_char_count[g_line] = 0;
+            at_begin = true;
             tmp += '\n';
         }
         else {
